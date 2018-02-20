34e30dd62c565cabde148c37d109b5406b52ebfc
Jaume Bonet <jaume.bonet@gmail.com>
2018-02-20 14:08:20 +0100

    Added positional similarity analysis

--- rstoolbox/analysis/__init__.py
+++ rstoolbox/analysis/__init__.py
@@ -1,2 +1,3 @@
-from sequence import pick_key_residues, sequence_similarity, sequence_frequency_matrix, binary_similarity, binary_overlap
+from sequence import pick_key_residues, sequence_similarity, positional_similarity
+from sequence import sequence_frequency_matrix, binary_similarity, binary_overlap
 from stats import cumulative
