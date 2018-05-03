866a47919905742f9964a40fec84e9eb5fefe167
Jaume Bonet <jaume.bonet@gmail.com>
2018-05-03 09:58:26 +0200

    improved test for utils.mutants

--- rstoolbox/tests/components/test_design.py
+++ rstoolbox/tests/components/test_design.py
@@ -3,7 +3,7 @@
 # @Email:  jaume.bonet@gmail.com
 # @Filename: test_design.py
 # @Last modified by:   bonet
-# @Last modified time: 02-May-2018
+# @Last modified time: 03-May-2018
 
 
 import os
@@ -18,6 +18,7 @@ import rstoolbox.io as ri
 import rstoolbox.components as rc
 import rstoolbox.plot as rp
 import rstoolbox.analysis as ra
+from rstoolbox.tests.helper import random_frequency_matrix
 
 
 class TestDesign( object ):
@@ -253,6 +254,18 @@ class TestDesign( object ):
         assert dfwt.shape[0] == 36
         assert 0 in dfwt.get_mutation_count('B').values
         assert refseq in dfwt.get_sequence('B').values
+
+        # Make mutants from Matrix
+        dfwt = rc.DesignFrame({"description": ["reference"], "sequence_B": [refseq]})
+        dfwt.add_reference_sequence('B', refseq)
+        matrix = random_frequency_matrix(len(df.get_reference_sequence('B')), 0)
+        key_res = [3, 5, 8, 12, 15, 19, 25, 27]
+        mutants = dfwt.generate_mutants_from_matrix('B', matrix, 5, key_res)
+        assert isinstance(mutants, list)
+        assert len(mutants) == 1
+        mutants = mutants[0].identify_mutants('B')
+        assert mutants.shape[0] == 5
+        assert mutants.pssm_score_B.mean() != 0
 
         # write to resfiles
         df.make_resfile("B", "NATAA", os.path.join(self.tmpdir, "mutanttest.resfile"))
